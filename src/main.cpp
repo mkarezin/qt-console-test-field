fcc86c1993c87999f65c72acf02606a988fa67c4
Mikhail Karezin <just_for_lolz@yahoo.com>
2024-06-25 22:07:51 +0300

    Пример вывода параметров доступных последовательных портов в отладочную консоль.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QList>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    return a.exec();
+    QList<QSerialPortInfo> portInfoList = QSerialPortInfo::availablePorts();
+
+    for (QSerialPortInfo &portInfo: portInfoList)
+    {
+        qDebug() << "portName: " << portInfo.portName() << "\n"
+                 << "systemLocation: " << portInfo.systemLocation() << "\n"
+                 << "description: " << portInfo.description() << "\n"
+                 << "manufacturer: " << portInfo.manufacturer() << "\n"
+                 << "serialNumber: " << portInfo.serialNumber() << "\n"
+                 << "vendorIdentifier: "
+                 << (portInfo.hasVendorIdentifier()
+                     ? QByteArray::number(portInfo.vendorIdentifier(), 16)
+                     : QByteArray()) << "\n"
+                 << "productIdentifier: "
+                 << (portInfo.hasProductIdentifier()
+                     ? QByteArray::number(portInfo.productIdentifier(), 16)
+                     : QByteArray()) << "\n";
+    }
+
+    return 0;
 }
